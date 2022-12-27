82c5978ef67551bb7f2dfef97114561f1dc20efb
Luis Felipe Mantoan <luisfelipemantoan@gmail.com>
2022-12-27 14:13:07 -0300

    Background color

--- app_1/index.js
+++ app_1/index.js
@@ -3,8 +3,24 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en" style="background-color: red;">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>APP - 1</title>
+  </head>
+  <body>
+  <strong>
+    <h1>APP_1</h1><br/>Tentando dar meu melhor na minha pior fase<br/>Sabe como é, menor<br/>Feridas se curam com o tempo, não com gaze<br/>
+  </strong>
+  </body>
+  </html>
+  `;
+  res.send(html)
   res.status(200)
-  res.send('<h1>APP_1</h1><br/>Tentando dar meu melhor na minha pior fase<br/>Sabe como é, menor<br/>Feridas se curam com o tempo, não com gaze<br/>')
 })
 
 app.get('/healthcheck', (req, res) => {
--- app_2/index.js
+++ app_2/index.js
@@ -3,7 +3,24 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>APP_2</h1><br/>I could talk about every time that you showed up on time<br/>But I\'d have an empty line \'cause you never did<br/>Never paid any mind to my mother or friends<br/>So I shut \'em all out for you \'cause I was a kid')
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en" style="background-color: green;">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>APP - 2</title>
+  </head>
+  <body>
+    <h1>APP_2</h1>
+    <strong>
+    <br/>I could talk about every time that you showed up on time<br/>But I\'d have an empty line \'cause you never did<br/>Never paid any mind to my mother or friends<br/>So I shut \'em all out for you \'cause I was a kid'
+    </strong>
+    </body>
+  </html>
+  `;
+  res.send(html)
   res.status(200)
 })
 
